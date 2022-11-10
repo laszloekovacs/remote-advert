50c105b71a8c532fbcf3ccbea65a15c1d6ffaa80
laszloekovacs <laszloekovacs@gmail.com>
2022-11-10 15:49:13 +0100

    fixed path issues of built project

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'docs' }
+  build: { outDir: 'docs' },
+  base: ''
 })
